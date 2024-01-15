fae9ee844311081365e62f844e6f15c1587b3cb4
Milind Singh <milind345@gmail.com>
2024-01-15 08:48:58 -0800

    Cleaning up

--- src/init.cpp
+++ src/init.cpp
@@ -1,3 +1,12 @@
+/**
+ * @file init.cpp
+ *
+ * @author Milind Singh (milind345@gmail.com)
+ *
+ * Initiliaze ESP BOT system software
+ *
+ */
+
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -16,6 +25,11 @@
 #include "devices/battery_mon.hpp"
 
 #include "logging.h"
+
+/* ========================================================================= */
+/* Definitions */
+/* ========================================================================= */
+
 LOG_TAG(INIT);
 
 #define MAIN_I2C_PORT I2C_NUM_1
@@ -23,14 +37,79 @@ LOG_TAG(INIT);
 #define MAIN_I2C_MASTER_SDA_PIN GPIO_NUM_13 /*!< I2C master SDA pin number */
 #define MAIN_I2C_MASTER_SCL_PIN GPIO_NUM_14 /*!< I2C master SCL pin number */
 
+/* ========================================================================= */
+/* ESP CONSOLE */
+/* ========================================================================= */
+
+namespace utils {
+
+class esp_bot_console
+{
+  private:
+    /* private data members */
+    std::unique_ptr<std::thread> _thread;
+
+    /* private API */
+    static void _run();
+
+  public:
+    esp_bot_console(void);
+};
+
+/**
+ * @brief
+ * @param
+ */
+void esp_bot_console::_run(void)
+{
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100u));
+    }
+}
+
+/**
+ * @brief
+ * @param
+ */
+esp_bot_console::esp_bot_console(void) {}
+
+} // namespace utils
+
+/* ========================================================================= */
+/* Static Members */
+/* ========================================================================= */
+
+static void inline _wdt_init();
+static void inline _banner(void);
+
 static std::unique_ptr<devices::battery_monitor> batt_mon{};
+static std::unique_ptr<utils::esp_bot_console> console{};
+
 static std::shared_ptr<comm::i2c_master_bus> i2c_main{};
+
+/* ========================================================================= */
+/* Private API */
+/* ========================================================================= */
+
+/**
+ * @brief Task watchdog init
+ */
+static void inline _wdt_init()
+{
+    /* configure the global timeout */
+    esp_task_wdt_config_t cfg = {
+        .timeout_ms = 1000u,
+        .idle_core_mask = 0x00,
+        .trigger_panic = true,
+    };
+    esp_task_wdt_init(&cfg);
+}
 
 /**
  * @brief display a welcome banner
  * @param
  */
-static void _banner(void)
+static void inline _banner(void)
 {
     printf("\t   -           __                       \n"
            "\t --          ~( @\\   \\                \n"
@@ -47,27 +126,21 @@ static void _banner(void)
            "                                                        \n");
 }
 
-/* Public API */
 /**
- * @brief
- * @param
+ * @brief Log version
  */
-void esp_bot_init(void)
+static void inline _version()
 {
-    esp_task_wdt_config_t cfg = {
-        .timeout_ms = 1000u,
-        .idle_core_mask = 0x00,
-        .trigger_panic = false,
-    };
-    esp_task_wdt_init(&cfg);
-
-    _banner();
-
     LOG_INFO("ESP-IDF Version: v%d.%d.%d", ESP_IDF_VERSION_MAJOR, ESP_IDF_VERSION_MINOR, ESP_IDF_VERSION_PATCH);
     LOG_INFO("ESP-BOT Version: v%s", "0.0.1");
+}
 
-    batt_mon = std::make_unique<devices::battery_monitor>(BATTERY_MON_ADC_CHANNEL);
-    i2c_main = std::make_shared<comm::i2c_master_bus>(MAIN_I2C_PORT, MAIN_I2C_MASTER_SDA_PIN, MAIN_I2C_MASTER_SCL_PIN);
+/* ========================================================================= */
+/* Public API */
+/* ========================================================================= */
+
+static void tb()
+{
     i2c_main->scan_devices();
 
     comm::i2c_slave_device dev(i2c_main, "PCA9685", 0x5Fu, 100u);
@@ -75,4 +148,28 @@ void esp_bot_init(void)
     if (dev.read_byte(0x00, val)) {
         dev.write_byte_rb(0x00, val | (0u << 4u), 100u);
     }
+}
+
+/**
+ * @brief
+ * @param
+ */
+void esp_bot_init(void)
+{
+    /* init ESP32 subsys */
+    _wdt_init();
+    _banner();
+    _version();
+
+    /* initialize battery monitor */
+    batt_mon = std::make_unique<devices::battery_monitor>(BATTERY_MON_ADC_CHANNEL);
+
+    /* initialize battery monitor */
+    console = std::make_unique<utils::esp_bot_console>();
+
+    /* initialize i2c master bus */
+    i2c_main = std::make_shared<comm::i2c_master_bus>(MAIN_I2C_PORT, MAIN_I2C_MASTER_SDA_PIN, MAIN_I2C_MASTER_SCL_PIN);
+
+    /* @todo remove after dev */
+    tb();
 }
